88771f90f79a7a13e60cfc76337a7d1dba2cd1bf
irisbune <irisbune@gmail.com>
2016-02-16 17:05:29 +0100

    signup en edit/update user works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user = User.update(user_params)
+      redirect_to @user
+    else
+      render edit_user_registration_path
+    end
+  end
+
+private
+  def user_params
+  params.require(:user).permit(:email, :password, :name, :city, :country, :gender, :profile_pic, :password_confirmation,  :current_password)
+  end
 end
