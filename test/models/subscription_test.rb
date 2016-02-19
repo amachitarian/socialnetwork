ef28a05808b7c4437c06f7cdbab88413c7684793
irisbune <irisbune@gmail.com>
2016-02-19 09:48:42 +0100

    adding follow ajax to user page and added posts feed to index and user pages

--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -2,22 +2,6 @@ require "test_helper"
 
 class Followers_Test < ActiveSupport::TestCase
 
-  def setup
-    @follower = Follower.new(follower_id: 1, followed_id: 2)
-  end
 
-  test "Should be valid" do
-    assert @follower.valid?
-  end
-
-  test "should require a follower_id" do
-    @follower.follower_id = nil
-    assert_not @follower.valid?
-  end
-
-  test "should require a followed_id" do
-    @follower.followed_id = nil
-    assert_not @follower.valid?
-  end
 
 end
