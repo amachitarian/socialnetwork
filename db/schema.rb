5490b8e074180e8d40d8b8f5b70513725012fce5
amachitarian <ashot_3@msn.com>
2016-02-19 10:30:33 +0100

    tests

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20160219072528) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "likes", force: :cascade do |t|
@@ -79,6 +80,7 @@ ActiveRecord::Schema.define(version: 20160219072528) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,30 @@
-require 'test_helper'
+  require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+include Devise::TestHelpers
+
+
+   setup do
+       @user = users(:one)
+     end
+
+     test "should get index" do
+       sign_in User.first
+       get :index
+       assert_response :success
+       assert_not_nil assigns(:users)
+     end
+
+       #
+      #  test "should show user" do
+      #       sign_in @user
+      #    get :show, id: @user
+      #    assert_response :success
+      #  end
+       #
+      #  test "should follow user" do
+      #       sign_in @user
+      #    post :follow, id: @user
+      #    assert_response :success
+      #  end
+  end
