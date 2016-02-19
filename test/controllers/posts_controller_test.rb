5c14ca00d2a9437a7323bfd9aa665834ebdf117f
amachitarian <ashot_3@msn.com>
2016-02-19 10:30:33 +0100

    tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+      @post = post(:one)
+    end
+
+    test "should get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:users)
 end
